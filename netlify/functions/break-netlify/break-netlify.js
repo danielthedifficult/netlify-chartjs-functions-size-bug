a77afb21ab968c7a7dd2477f9b03ff21a5b2d870
Daniel Mulroy <dan@danmulroy.com>
2022-09-26 19:07:48 +0200

    init

---
+++ netlify/functions/break-netlify/break-netlify.js
@@ -0,0 +1,19 @@
+const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
+
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const handler = async (event) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Please make this work!` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
